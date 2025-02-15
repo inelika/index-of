61c7de3c3179cd4560919cd3ecbe5a18c2e9be20
inelika <dosanenlik@gmail.com>
2025-02-15 14:44:06 +0500

    Create indexOf.js

---
+++ indexOf.js
@@ -0,0 +1,12 @@
+const paragrah = "the quiek brown fox jumps over the lazy dog. If the dog barked , was a really lazy?"
+const serchTerm ="dog";
+function findOccurrences(text, word, startIndex = 0) {
+    let index = text.indexOf(word, startIndex);
+    if (index !== -1) {
+        console.log(`Find: "${word}" at index ${index}`);
+        findOccurrences(text, word, index + word.length); 
+    }
+}
+
+
+findOccurrences(paragraph, searchTerm);
