84b2b4857a8cef548abfcd39ddabab63b2f68141
inelika <dosanenlik@gmail.com>
2025-03-03 07:27:40 +0500

    Update indexOf.js

--- indexOf.js
+++ indexOf.js
@@ -1,12 +1,12 @@
-const paragrah = "the quiek brown fox jumps over the lazy dog. If the dog barked , was a really lazy?"
-const serchTerm ="dog";
+const paragraph = "The quick brown fox jumps over the lazy dog. If the dog barked, was it really lazy?";
+const searchTerm = "dog";
+
 function findOccurrences(text, word, startIndex = 0) {
-    let index = text.indexOf(word, startIndex);
+    let index = text.toLowerCase().indexOf(word.toLowerCase(), startIndex);
     if (index !== -1) {
-        console.log(`Find: "${word}" at index ${index}`);
-        findOccurrences(text, word, index + word.length); 
+        console.log(⁠ Found: "${word}" at index ${index} ⁠);
+        findOccurrences(text, word, index + word.length);
     }
 }
-
 
 findOccurrences(paragraph, searchTerm);
